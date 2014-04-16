458ed9c115f674d1e7bca14258298b7c49dea4b9
Julien Ogier <julien@julien-fixe.home>
2014-04-16 16:49:09 +0200

    Premier commit

---
+++ desktop/src/net/kehkklan/towerdefense/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package net.kehkklan.towerdefense.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import net.kehkklan.towerdefense.TDGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new TDGame(), config);
+	}
+}
---
+++ html/src/net/kehkklan/towerdefense/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package net.kehkklan.towerdefense.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import net.kehkklan.towerdefense.TDGame;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener getApplicationListener () {
+                return new TDGame();
+        }
+}
